445af8c89aa9281cefd1bb4217e1d472b99cd05e
wyy <wuyanyi09@gmail.com>
2014-05-19 11:39:32 +0800

    add footer

--- start.sh
+++ start.sh
@@ -1,23 +1,5 @@
 #!/bin/sh
 
-PATH=/usr/bin/:/usr/local/bin/:/sbin/:$PATH
 
 LANG="en_US.UTF-8"; LC_ALL="en_US.UTF-8"; 
-
-PID=`pidof cjserver`
-
-LOGFILE=run.log
-
-if [ ! -z "${PID}" ]
-then
-    kill ${PID}
-    sleep 1
-    echo "kill ${PID}"
-    jekyll serve --port 80 >> $LOGFILE 2>&1 &
-    echo "jekyll server started."
-else
-    jekyll serve --port 80 >> $LOGFILE 2>&1 &
-    echo "jekyll server started."
-fi
-
-    
+jekyll serve --port 80 
